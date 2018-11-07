508c3ac6efea0b0c66f63015974c4083550ebde8
Matthew Agius <mtagius@gmail.com>
2018-11-07 12:52:31 -0500

    I got developer access!

--- open-tabs.bat
+++ open-tabs.bat
@@ -6,7 +6,8 @@ set list=^
  "https://github.com/mtagius/twitter-avatar-changer"^
  "https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile_image.html"^
  "https://twitter.com/eigenbom"^
- "https://twitter.com/mtagius_"
+ "https://twitter.com/mtagius_"^
+ "https://developer.twitter.com/en/account/get-started"
 for %%a in (%list%) do ( 
    start chrome %%a 
 )
