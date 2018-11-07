915af2865e4138006f70ff95c890aab76e19eeb3
Matthew Agius <mtagius@gmail.com>
2018-11-06 19:18:12 -0500

    added website to list of tabs

--- open-tabs.bat
+++ open-tabs.bat
@@ -3,6 +3,7 @@ start chrome
 sleeep1
 
 set list=^
+ "https://github.com/mtagius/twitter-avatar-changer"^
  "https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile_image.html"^
  "https://twitter.com/eigenbom"^
  "https://twitter.com/mtagius_"
